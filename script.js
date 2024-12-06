0952dcd60416780f364ca97959f9a0284075fc5f
JcksonMCD <jacksonmacdonald03@gmail.com>
2024-12-06 10:10:21 +0000

    Feat: first button text saved in variable.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('hello from index.js!');
 
 const timeRemaining = document.getElementById("time-remaining");
-console.log(timeRemaining.textContent);
+const add30sButton = document.getElementById("add-30s-button");
+console.log(add30sButton.textContent)
