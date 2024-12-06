992b5dfa010cb2c9f00e23b05f5f82412d70cc6e
JcksonMCD <jacksonmacdonald03@gmail.com>
2024-12-06 10:19:27 +0000

    Feat: On click listner functionality implemented to update Time remaining.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 console.log('hello from index.js!');
 
-const timeRemaining = document.getElementById("time-remaining");
+let timeRemaining = document.getElementById("time-remaining");
 const add30sButton = document.getElementById("add-30s-button");
 
-const handleClick = (e) => {
-    console.log('I was clicked');
+const add30ToTime = (e) => {
+    timeRemaining.textContent = Number(timeRemaining.textContent) + 30;
+    console.log(timeRemaining)
 }
-add30sButton.addEventListener("click", handleClick);
+add30sButton.addEventListener("click", add30ToTime);
