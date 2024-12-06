83385f69fd97c0d1c7a4c61cc0475f181f3cff80
JcksonMCD <jacksonmacdonald03@gmail.com>
2024-12-06 14:18:13 +0000

    Feat: Timer ticks down but is buggy.

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ console.log('hello from index.js!');
 
 let timeRemaining = document.getElementById("time-remaining");
 const add30sButton = document.getElementById("add-30s-button");
+const startButton = document.getElementById("start-button");
+console.log(startButton.textContent)
 
 const add30ToTime = (e) => {
     timeRemaining.textContent = Number(timeRemaining.textContent) + 30;
     console.log(timeRemaining)
 }
 add30sButton.addEventListener("click", add30ToTime);
+
+const startCountdown = (e) => {
+    if (Number(timeRemaining.textContent > 1)){
+        timeRemaining.textContent = Number(timeRemaining.textContent) - 1;
+    }
+}
+const interval = setInterval(startCountdown, 1000);
+
+startButton.addEventListener("click", startCountdown);
